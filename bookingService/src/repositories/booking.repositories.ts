3922903dc4cb787a49de30520d5e0905f85fa59d
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-06-24 23:09:09 +0530

    error in idem_key

--- bookingService/src/repositories/booking.repositories.ts
+++ bookingService/src/repositories/booking.repositories.ts
@@ -2,10 +2,25 @@ import { Prisma } from "@prisma/client";
 
 import prismaClient from "../prisma/client";
 
+
 export async function createBooking (bookingInput:Prisma.BookingCreateInput){
     const booking = await prismaClient.booking.create({
         data:bookingInput
     });
     return booking;
 
+}
+
+export async function createIdempotencyKey(key:string,bookingId:number) {
+    const idempotencyKey = await prismaClient.idempotencyKey.create({
+        data:{
+            key,
+            booking:{
+                connect:{
+                    id:bookingId
+                }
+            }
+        }
+    });
+    return idempotencyKey;
 }
